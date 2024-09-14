320241a71b911fb8e1c31cd7bbd36c0fe7c6aa97
Subhash Surana <subhash.surana23@gmail.com>
2024-09-14 12:43:47 +0530

    Added CodeDeploy Alarms for monitoring Blue-Green Deployment

--- lib/lambda-greeting-api-stack.ts
+++ lib/lambda-greeting-api-stack.ts
@@ -8,6 +8,8 @@ import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as subscription from 'aws-cdk-lib/aws-sns-subscriptions';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as cloudwatchActions from 'aws-cdk-lib/aws-cloudwatch-actions';
 import path = require('path');
 
 
@@ -24,10 +26,10 @@ export class LambdaGreetingApiStack extends cdk.Stack {
 
 
     // "Greeting" SNS Topic
-    const greetingsTopic = new sns.Topic(this, `GreetingsTopic${env_suffix}`,);
+    const greetingsTopic = new sns.Topic(this, `GreetingsTopic-${env_suffix}`,);
 
     // "Greeting" SQS Topic
-    const greetingsQueue = new sqs.Queue(this, `GreetingsQueue${env_suffix}`,{
+    const greetingsQueue = new sqs.Queue(this, `GreetingsQueue-${env_suffix}`,{
       visibilityTimeout: cdk.Duration.seconds(300)
     });
 
@@ -47,6 +49,7 @@ export class LambdaGreetingApiStack extends cdk.Stack {
         SNS_TOPIC_ARN: greetingsTopic.topicArn
       },
       memorySize: environmentType === 'production' ? 1024 : 512,  // Different memory size based on environment
+      description: `Lambda Function for creating random Greetings - ${env_suffix}`
     });
 
     // Create Lambda version & alias for Blue-Green Deployment
@@ -65,7 +68,7 @@ export class LambdaGreetingApiStack extends cdk.Stack {
       handler: greetingLambda,
       proxy: false,
       restApiName: `lambdaGreetingApi-${env_suffix}`,
-      description: "API fo Lambda Greeting Function",
+      description: `API fo Lambda Greeting Function - ${env_suffix}`,
       deployOptions: {
         stageName: `${env_suffix}`
       }
@@ -88,6 +91,37 @@ export class LambdaGreetingApiStack extends cdk.Stack {
         failedDeployment: true,
       },
     });
+
+    // SNS Topic for CodeDeploy Failure Notifications
+    const alarmTopic = new sns.Topic(this, `AlarmTopic-${env_suffix}`, {
+      displayName: `CodeDeploy Failure Notifications - ${env_suffix}`,
+    });
+
+    // Add subscription to the SNS topic (e.g., email)
+    alarmTopic.addSubscription(
+      new subscription.EmailSubscription('jemega6074@ofionk.com')
+    );
+
+    // Create a CloudWatch alarm for CodeDeploy deployment failures
+    const greetingLambdaDeploymentFailuresAlarm  = new cloudwatch.Alarm(this, `GreetingLambdaDeploymentFailuresAlarm-${env_suffix}`, {
+      metric: new cloudwatch.Metric({
+        namespace: 'AWS/CodeDeploy',
+        metricName: 'DeploymentSuccess',
+        dimensionsMap: {
+          DeploymentGroupName: `GreetingLambdaDeploymentGroup-${env_suffix}`,
+        },
+        statistic: 'Sum',
+        period: cdk.Duration.minutes(1),
+      }),
+      threshold: 1,  // Alarm if success metric is less than 1
+      evaluationPeriods: 1,
+      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
+      alarmDescription: `Alarm if the deployment of GreetingLambda in ${env_suffix} fails`,
+    });
+    
+    // Add SNS action for notifications on deployment failure
+    greetingLambdaDeploymentFailuresAlarm.addAlarmAction(new cloudwatchActions.SnsAction(alarmTopic));
+
 
     // Another Lambda triggered by SQS Queue
     const sqsConsumerLambda = new NodejsFunction(this, `sqsConsumerLambda-${env_suffix}`, {
@@ -119,15 +153,67 @@ export class LambdaGreetingApiStack extends cdk.Stack {
       },
     });
 
-    new cdk.CfnOutput(this, `GreetLambdaFunction-${env_suffix}`, {
+    // Create CodeDeploy CloudWatch Alarm for SQSConsumerLambda with prefix
+    const sqsConsumerLambdaDeploymentFailuresAlarm = new cloudwatch.Alarm(this, `S-QSConsumerLambdaDeploymentFailuresAlarm-${env_suffix}`, {
+      metric: new cloudwatch.Metric({
+        namespace: 'AWS/CodeDeploy',
+        metricName: 'DeploymentSuccess',
+        dimensionsMap: {
+          DeploymentGroupName: `SQSConsumerLambdaDeploymentGroup-${env_suffix}`,
+        },
+        statistic: 'Sum',
+        period: cdk.Duration.minutes(1),
+      }),
+      threshold: 1,
+      evaluationPeriods: 1,
+      comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
+      alarmDescription: `Alarm if the deployment of SQSConsumerLambda in - ${env_suffix} fails`,
+    });
+    sqsConsumerLambdaDeploymentFailuresAlarm.addAlarmAction(new cloudwatchActions.SnsAction(alarmTopic));
+
+
+    // CfnOutput: Expose key resources
+    new cdk.CfnOutput(this, `lambdaGreetingOutput-${env_suffix}`, {
       value: greetingLambda.functionName,
-      description: 'JavaScript Lambda function'
+      description: 'Typescript Lambda function for generating random Greetings'
+    });
+
+    new cdk.CfnOutput(this, `sqsConsumerLambdaOutput-${env_suffix}`, {
+      value: sqsConsumerLambda.functionName,
+      description: 'Typescript Lambda function for consuming random Greetings message from SQS Queue'
     });
 
     new cdk.CfnOutput(this, `lambdaGreetingApiEndpoint-${env_suffix}`, {
       value: apigw.url + 'greeting',
       description: 'The API Gateway endpoint for the /greeting resource',
     });
+
+    new cdk.CfnOutput(this, `SnsGreetingsTopicName-${env_suffix}`, {
+      value: greetingsTopic.topicName,
+      description: `SNS Topic Name for Greetings ${env_suffix}`,
+    });
+
+    new cdk.CfnOutput(this, `SnsCodeDeployAlarmTopicName-${env_suffix}`, {
+      value: alarmTopic.topicName,
+      description: `SNS Topic Name for CodeDeploy Failure Alarm ${env_suffix}`,
+    });
+
+    new cdk.CfnOutput(this, `SqsQueueUrl-${env_suffix}`, {
+      value: greetingsQueue.queueUrl,
+      description: `$SQS Queue URL for Greetings ${env_suffix}`,
+    });
+
+    new cdk.CfnOutput(this, `GreetingLambdaDeploymentAlarm-${env_suffix}`, {
+      value: `https://console.aws.amazon.com/cloudwatch/home?region=${this.region}#alarmsV2:alarm/${greetingLambdaDeploymentFailuresAlarm.alarmName}`,
+      description: `CloudWatch Alarm URL for GreetingLambda Deployment ${env_suffix}`,
+    });
+
+    new cdk.CfnOutput(this, `SQSConsumerLambdaDeploymentAlarm-${env_suffix}`, {
+      value: `https://console.aws.amazon.com/cloudwatch/home?region=${this.region}#alarmsV2:alarm/${sqsConsumerLambdaDeploymentFailuresAlarm.alarmName}`,
+      description: `CloudWatch Alarm URL for SQSConsumerLambda Deployment ${env_suffix}`,
+    });
+
+
 
 
   }
